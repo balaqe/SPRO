d9bab60d2968b915910567c18760f465c88204b3
sam223221 <58514851+sam223221@users.noreply.github.com>
2022-12-04 04:06:48 +0100

    total rewrite for the lcd
    there has been a tottal rewrite because i wanted to configurate the car real time and now it should work

--- display.c
+++ display.c
@@ -3,20 +3,20 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "usart.h"
+#include "lcd.h"
+#include "i2cmaster.h"
 
 #define F_CPU 16000000UL
 #define BAUD 9600
 
 char ReadBuffer[8];
-int carSpeed = 0;
-int secounds = 0; 
+char Input_carSpeed;
+char Input_sec; 
 int counter = 5;
 int Interval = 0;
-
-
-
-
-
+int time;
+int value;
+char navn;
 
 
 
@@ -26,16 +26,19 @@ int main(void)
     
     uart_init(); // open the communication to the microcontroller
 	io_redirect(); // redirect input and output to the communication
+    printf("page 0%c%c%c",255,255,255);
 
-    printf("page 0%c%c%c",255,255,255);     //going to homepage on the screen so it know where it starts
-
+    
+    
     while(1) {
+        
+        
 
- 
-        CarStartup();                       //Calls a fuction carstartup
+        Button_scaner(7);
+        Car_config();                   // configurates the car
+        CarStartup();                   //Calls a fuction carstartup
 
     }
-        
     return 0;
 }
 
@@ -46,91 +49,120 @@ int main(void)
 
 
 
-int CarStartup()
-{
 
-    for(int i = 0; i<8;i++)                 // loop that has to go throw the Serial to check if there is a putton press
-    {
-        scanf("%c", &ReadBuffer[i]);        // Scanse the Serial port for info
+
+
+
+
+
+
+void Car_config(){
+    
+    if (ReadBuffer[0] == 0x65 &&  ReadBuffer[1] == 0x03 && ReadBuffer[2] == 0x05 && ReadBuffer[3] == 0x01){
+        Button_scaner(8);
+
+        Input_sec = ReadBuffer[0];
+        Input_carSpeed = ReadBuffer[4];
+
     }
+    if (ReadBuffer[0] == 0x65 &&  ReadBuffer[1] == 0x03 && ReadBuffer[2] == 0x04 && ReadBuffer[3] == 0x00){
 
-        if(ReadBuffer[0] == 0x65 )           //  Selected program 1 for the car to run on
-        {
-
-            counter = 5;                    // sets counter to 5 sec
-
-            for (int i = 0; i < 6; i++)     // this does the count down on the screen
-            {
-                    
-                printf("page2.n2.val=%d%c%c%c",counter,255,255,255);    //sends the vaible to the screen so it can show the count down
-                counter--;                  // decreses the counter by 1
-                _delay_ms(1000);            // makes a delay of 1 sec
-
-            }
-
-            CarRun();                       // Calls the fuction CarRun that Runs the car
-            
-
-        }
-        
+        printf("x0.val=%d%c%c%c",Input_carSpeed,255,255,255);
+        printf("n0.val=%d%c%c%c",Input_sec,255,255,255);
+    }    
     
 
 }
 
-int CarRun()
-{
-    carSpeed = 60;                  // sets the Carspeed for the car
-    secounds = 30;                  // sets the sec for the interval
-    Interval = 1;
 
-    printf("page2.n1.val=%d%c%c%c",Interval,255,255,255);      // sets the Interval number to 1
-    
-    for (int i = 1; secounds+1 > i ; i++)               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void Button_scaner(int a){
+
+
+    for(int i = 0; i<a;i++)                 // loop that has to go throw the Serial to check if there is a putton press
+    {
+        scanf("%c", &ReadBuffer[i]);        // Scanse the Serial port for info
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+void CarStartup()
+{
+
+    if(ReadBuffer[0] == 0x65 &&  ReadBuffer[1] == 0x01 && ReadBuffer[2] == 0x02 && ReadBuffer[3] == 0x00)           //  Selected program 1 for the car to run on
     {
 
-        printf("page2.z0.val=%d%c%c%c",i*6,255,255,255);        // sends the sec valu to the clock, it has to be *6 because one reveloution is 360
-        printf("page2.n0.val=%d%c%c%c",i,255,255,255);          // sends the sec valu to the interger n0 on the display
-        printf("page2.x0.val=%d%c%c%c",carSpeed,255,255,255);   // sends the Carspeed to the dispay interger x0
-        printf("page2.z1.val=%d%c%c%c",carSpeed,255,255,255);   // sends the Carspeed to the dispay interger z1
+    counter = 5;                    // sets counter to 5 sec
+
+    for (int i = 0; i < 6; i++)     // this does the count down on the screen
+    {
+        printf("n2.val=%d%c%c%c",counter,255,255,255);    //sends the vaible to the screen so it can show the count down
+        counter--;                  // decreses the counter by 1
+        printf("n0.val=%d%c%c%c",Input_sec,255,255,255);
+        _delay_ms(1000);
+
+    }
+
+    CarRun();                       // Calls the fuction CarRun that Runs the car        
+   
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+void CarRun()
+{
+
+    // while (timer < Input_sec){
+    for (int i = 1; Input_sec + 1 > i ; i++)               
+    {
+
+        printf("z0.val=%d%c%c%c",i*6,255,255,255);        // sends the sec valu to the clock, it has to be *6 because one reveloution is 360
+        printf("n0.val=%d%c%c%c",i,255,255,255);          // sends the sec valu to the interger n0 on the display
+        printf("x0.val=%d%c%c%c",Input_carSpeed,255,255,255);   // sends the Carspeed to the dispay interger x0
+        printf("z1.val=%d%c%c%c",Input_carSpeed,255,255,255);   // sends the Carspeed to the dispay interger z1
         
         
         _delay_ms(1000);                // delay of 1 sec
         
     }
-    carSpeed = 80;                  // sets the Carspeed for the car
-    secounds = 20;                  // sets the sec for the interval
-    Interval = 2;
-
-    printf("page2.n1.val=%d%c%c%c",Interval,255,255,255);              // sets the interval nr too 2
-
-
-    for (int i = 1; secounds+1 > i ; i++)
-    {
-
-        printf("page2.z0.val=%d%c%c%c",i*6,255,255,255);        // sends the sec valu to the clock, it has to be *6 because one reveloution is 360
-        printf("page2.n0.val=%d%c%c%c",i,255,255,255);          // sends the sec valu to the interger n0 on the display
-        printf("page2.x0.val=%d%c%c%c",carSpeed,255,255,255);   // sends the Carspeed to the dispay interger x0
-        printf("page2.z1.val=%d%c%c%c",carSpeed,255,255,255);   // sends the Carspeed to the dispay interger z1
-        
-        
-        _delay_ms(1000);// delay of 1 sec
-        
-    }
-
-    _delay_ms(3000);                        // Makes a delay of 3 sec before finiching the program
-    CarReset();
-    printf("page 1%c%c%c",255,255,255);
-
     
-}
-
-
-int CarReset()
-{
-
-    carSpeed = 0;
-    secounds = 0;
-    Interval = 0;
-    counter = 5;
+    printf("page 1%c%c%c",255,255,255);
+    _delay_ms(2000);
 
 }
