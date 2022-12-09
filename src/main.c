9b3662ce2167769c62a5634a536c27011fe95e26
Marcus Kaa <mkaa2002@gmail.com>
2022-12-09 12:19:31 +0100

    did some stuff
    should be the final version of the code

--- src/main.c
+++ src/main.c
@@ -6,33 +6,38 @@
 
 #include "usart.h"
 
-void time() {
+void time()
+{
   unsigned int time;
-  TIFR1 = (1 << ICF1); // resets the timer interupt flag register so it no longer is on rising edge
-  TCNT1 = 0; // resets the timer
-  time = ICR1; // copies the interupt capture register into a local variable
-  printf("\n time is: %d", time); // prints out the time (needs to be changed to lcd)
+  TIFR1 = (1 << ICF1);  // resets the timer interupt flag register so it no longer is on rising edge
+  TCNT1 = 0;            // resets the timer
+  time = ICR1;          // copies the interupt capture register into a local variable
+  printf("\n%d", time); // prints out the time (needs to be changed to lcd)
 }
 
-void motor() {
+void motor()
+{
   int count = 0;
-  while (count > 255) {// 255 is the time period - for the pwm calculator
+  while (count < 255)
+  { // 255 is the time period - for the pwm calculator
 
-    if (TIFR1 & (1 << ICF1)) { // checks if there is a rising edge AKA the optocoupler has been interrupted, if so it gets the time 
+    if (TIFR1 & (1 << ICF1))
+    {         // checks if there is a rising edge AKA the optocoupler has been interrupted, if so it gets the time
       time(); // function for optocoupler
     }
-    
+
     count = count + 1;
-    if (count > OCR0A) {
+    if (count > OCR0A)
+    {
       PORTD |= (1 << PD6); // sets a logic 1 to our pwm pin
       count = 0;
     }
-    else {
+    else
+    {
       PORTD &= ~(1 << PD6); // sets a logic 0 to our pwm pin
     }
   }
 }
-
 
 int main(void)
 {
@@ -40,20 +45,18 @@ int main(void)
   uart_init();   // open the communication to the microcontroller
   io_redirect(); // redirect input and output to the communication
   DDRD |= 0x60;
-  OCR0A = 170; // pulse width - change this to controle speed
+  OCR0A = 255; // pulse width - change this to controle speed
   // for testing
-  //int count = 220;
-  //OCR0A = 220;
-  //int count2 = 0;
-
-  //OCR0A = 100;
+  // int count = 220;
+  // OCR0A = 220;
+  // int count2 = 0;
+  // OCR0A = 100;
   TCCR0A |= 0xA3;
   TCCR0B |= 0x05;
   TCCR1A = 0x00;
   DDRB &= ~0x01;
   PORTB |= 0x01;
   TCCR1B = (1 << ICNC1) | (1 << ICES1) | (1 << CS12) | (1 << CS10);
-
 
   while (1)
   {
@@ -75,7 +78,6 @@ int main(void)
     //  }
 
     motor(); // calls the motor function, made so its sohuld be easier to combine lcd code
-    
   }
   return 0;
 }
